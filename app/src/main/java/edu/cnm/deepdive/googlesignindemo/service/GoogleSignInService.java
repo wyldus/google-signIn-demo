5f83be75c3670f27f9e676200d612fe9682e1c5a
dagoberto <berto35572@gmail.com>
2021-07-14 10:30:08 -0600

    Completed sign-in service(w/o reactivex support)

--- app/src/main/java/edu/cnm/deepdive/googlesignindemo/service/GoogleSignInService.java
+++ app/src/main/java/edu/cnm/deepdive/googlesignindemo/service/GoogleSignInService.java
@@ -1,7 +1,85 @@
 package edu.cnm.deepdive.googlesignindemo.service;
 
+import android.app.Activity;
+import android.app.Application;
+import android.content.Intent;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 public class GoogleSignInService {
 
+  private static Application context;
+
+  private final GoogleSignInClient client;
+
+  private GoogleSignInAccount account;
+
+  private GoogleSignInService() {
+    GoogleSignInOptions options = new GoogleSignInOptions.Builder()
+        .requestEmail()
+        .requestId()
+        .requestProfile()
+//        .requestIdToken(BuildConfig.CLIENT_ID)
+        .build();
+    client = GoogleSignIn.getClient(context, options);
+
+  }
+
+  public static void setContext(Application context) {
+    GoogleSignInService.context = context;
+  }
 
 
+  public static GoogleSignInService getInstance() {
+    return InstanceHolder.INSTANCE;
+  }
+
+  public static Application getContext() {
+    return context;
+  }
+
+  public GoogleSignInClient getClient() {
+    return client;
+  }
+
+  public GoogleSignInAccount getAccount() {
+    return account;
+  }
+
+  public Task<GoogleSignInAccount> refresh() {
+    return client.silentSignIn()
+        .addOnSuccessListener((account) -> this.account = account);
+  }
+
+  public void startSignIn(Activity activity, int requestCode) {
+    account = null;
+    Intent intent = client.getSignInIntent();
+    activity.startActivityForResult(intent, requestCode);
+  }
+
+  public Task<GoogleSignInAccount> completeSignIn(Intent data) {
+    Task<GoogleSignInAccount> task = null;
+    try {
+      task = GoogleSignIn.getSignedInAccountFromIntent(data);
+      account = task.getResult(ApiException.class);
+    } catch (ApiException e) {
+// Exception will be passed automatically to onFailureListener
+    }
+    return task;
+  }
+
+  public Task<Void> signOut(){
+    return client.signOut()
+        .addOnCompleteListener((ignored)-> account = null);
+  }
+  private static class InstanceHolder {
+
+    private static final GoogleSignInService INSTANCE = new GoogleSignInService();
+
+  }
 }
+
