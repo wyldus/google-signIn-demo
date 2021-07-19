9588074c93de710ad20e567e9daddf07820fc4b8
dagoberto <berto35572@gmail.com>
2021-07-19 09:02:49 -0600

    Logged token.

--- app/src/main/java/edu/cnm/deepdive/googlesignindemo/service/GoogleSignInService.java
+++ app/src/main/java/edu/cnm/deepdive/googlesignindemo/service/GoogleSignInService.java
@@ -3,12 +3,14 @@ package edu.cnm.deepdive.googlesignindemo.service;
 import android.app.Activity;
 import android.app.Application;
 import android.content.Intent;
+import android.util.Log;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import edu.cnm.deepdive.googlesignindemo.BuildConfig;
 
 public class GoogleSignInService {
 
@@ -23,7 +25,7 @@ public class GoogleSignInService {
         .requestEmail()
         .requestId()
         .requestProfile()
-//        .requestIdToken(BuildConfig.CLIENT_ID)
+        .requestIdToken(BuildConfig.CLIENT_ID)
         .build();
     client = GoogleSignIn.getClient(context, options);
 
@@ -52,7 +54,7 @@ public class GoogleSignInService {
 
   public Task<GoogleSignInAccount> refresh() {
     return client.silentSignIn()
-        .addOnSuccessListener((account) -> this.account = account);
+        .addOnSuccessListener(this::setAccount);
   }
 
   public void startSignIn(Activity activity, int requestCode) {
@@ -65,7 +67,7 @@ public class GoogleSignInService {
     Task<GoogleSignInAccount> task = null;
     try {
       task = GoogleSignIn.getSignedInAccountFromIntent(data);
-      account = task.getResult(ApiException.class);
+      setAccount(task.getResult(ApiException.class));
     } catch (ApiException e) {
 // Exception will be passed automatically to onFailureListener
     }
@@ -73,10 +75,18 @@ public class GoogleSignInService {
   }
 
   //sign out method
-  public Task<Void> signOut(){
+  public Task<Void> signOut() {
     return client.signOut()
-        .addOnCompleteListener((ignored)-> account = null);
+        .addOnCompleteListener((ignored) -> setAccount(null));
   }
+
+  private void setAccount(GoogleSignInAccount account) {
+    this.account = account;
+    if (account != null) {
+      Log.d(getClass().getSimpleName(), account.getIdToken());
+    }
+  }
+
   private static class InstanceHolder {
 
     private static final GoogleSignInService INSTANCE = new GoogleSignInService();
